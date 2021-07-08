9f2795a4a53afd5f4561ccecaad55ecf05213804
ashutosh-chahar <ashutoshchahar264@gmail.com>
2021-07-09 01:29:40 +0530

    ++

--- EmailNotificationService/EmailNotificationService/emailService/utils.py
+++ EmailNotificationService/EmailNotificationService/emailService/utils.py
@@ -7,13 +7,3 @@ def sendEmailNotification():
     recepient = str("someeami@gmail.com")
     send_mail(subject,message, EMAIL_HOST_USER, [recepient], fail_silently=True)
 
-
-def send_email():
-    email = EmailMessage(
-        'Title',
-        (ConsultSerializer.name, ConsultSerializer.email, ConsultSerializer.phone),
-        'my-email',
-        ['my-receive-email']
-    )
-    email.attach_file(ConsultSerializer.file)
-    email.send()
